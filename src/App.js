1451e9b9702e4b122c4e808040f9425270ad7499
Gabriel RICARD <gabriel.ricard@capgemini.com>
2024-10-15 08:16:32 +0200

    CACA

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MUIButton></MUIButton>
+        <MUIButton>CACA</MUIButton>
       </header>
     </div>
   );
